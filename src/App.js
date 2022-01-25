0768d594e936899cc6d328afe4146dd2053c67f3
UIT20521569 <20521569@gm.uit.edu.vn>
2022-01-25 23:36:46 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react'
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
+  const adminUser = {
+    email: "huulongn15@gmail.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({ name: "", email: "" })
+  const [error, setError] = useState("")
+
+  const Login = details => {
+    console.log(details);
+    if (details.email === adminUser.email && details.password === adminUser.password) {
+      console.log('Logged in');
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    } else {
+      setError('The email address or password is incorrect.')
+    }
+  }
+  const Logout = () => {
+    setUser({
+      name: '',
+      email: ''
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        (user.email !== "") ? (
+          <div>
+            <h2 className="text-purple-500 text-[18px] font-bold mt-12">
+            Welcome, <span>{user.name}</span>
+            </h2>
+            <button
+              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded my-6"
+              onClick={Logout}>
+              Logout
+            </button>
+          </div>
+        ) :
+          <LoginForm Login={Login} error={error} />
+      }
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { useState } from 'react';
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ name: "", email: "", password: "", })
+
+  const submitHanler = e => {
+    e.preventDefault();
+    Login(details)
+  }
+
+  return (
+    <form className="w-full max-w-sm mt-6 flex mx-auto flex-col">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+            Full Name
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-full-name"
+            type="text"
+            placeholder="Nguyen Huu Long"
+            onChange={e => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-email">
+            Email
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-email"
+            type="text"
+            placeholder="huulongn15@gmail.com"
+            onChange={e => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password">
+            Password
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-password"
+            type="password"
+            placeholder="******************"
+            onChange={e => setDetails({ ...details, password: e.target.value })}
+            value={details.password}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button
+            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+            onClick={submitHanler}
+          >
+            LOGIN
+          </button>
+        </div>
+      </div>
+      {(error !== '') ? (
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3 my-3 text-red-600 text-[18px] font-bold">
+            {error}
+          </div>
+        </div>
+      ) : ''}
+    </form>
+  )
+}
+
+export default LoginForm;
